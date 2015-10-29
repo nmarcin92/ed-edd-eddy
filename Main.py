2b9fbdc0a4c3b1c0fc5b50c8a50f80d06de1ae5d
Ucash <ucashek@gmail.com>
2015-10-29 17:20:12 +0100

    add exception handling

--- Main.py
+++ Main.py
@@ -7,9 +7,10 @@ if __name__ == "__main__":
     database = Database()
     listener = TweeterStreamListener(database)
     stream = tweepy.Stream(auth=api.auth, listener=listener)
-    # while True:
-    #     try:
-    stream.filter(track=database.find_all_keyword())
-    # except Exception as e:
-    #     print e
-    #     print 'error'
+    while True:
+        try:
+            stream.filter(track=database.find_all_keyword())
+        except Exception as e:
+            print 'error'
+            print e
+            print "****************"
