4b97086cecd66f8f33ad0b24f7e0b9b17c6c5008
Ucash <ucashek@gmail.com>
2015-12-01 20:39:47 +0100

    add limit

--- user_graph/Database.py
+++ user_graph/Database.py
@@ -5,30 +5,35 @@ class Database:
     def __init__(self):
         self.connection = psycopg2.connect("dbname=twitter user=postgres password=lolol123")
 
-    def get_users_in_reply_to_connection(self):
+    def get_users_in_reply_to_connection(self, limit):
         cur = self.connection.cursor()
         cur.execute("""
             SELECT u.username, u2.username FROM twitter_user u
             JOIN tweet t ON t.user_id = u.id
             JOIN tweet t2 ON t2.id = t.in_reply_to
-            JOIN twitter_user u2 ON t2.user_id = u2.id""")
+            JOIN twitter_user u2 ON t2.user_id = u2.id
+            ORDER BY u.followers_count DESC
+            LIMIT %d""", (limit, ))
         return cur.fetchall()
 
-    def get_users_retweet_connection(self):
+    def get_users_retweet_connection(self, limit):
         cur = self.connection.cursor()
         cur.execute("""
             SELECT u.username, u2.username FROM twitter_user u
             JOIN tweet t ON t.user_id = u.id
             JOIN tweet t2 ON t2.id = t.retweet_id
-            JOIN twitter_user u2 ON t2.user_id = u2.id""")
+            JOIN twitter_user u2 ON t2.user_id = u2.id
+            ORDER BY u.followers_count DESC
+            LIMIT %d""", (limit, ))
         return cur.fetchall()
 
-    def get_users_retweet_or_reply_to_connection(self):
+    def get_users_retweet_or_reply_to_connection(self, limit):
         cur = self.connection.cursor()
         cur.execute("""
             SELECT u.username, u2.username FROM twitter_user u
             JOIN tweet t ON t.user_id = u.id
             JOIN tweet t2 ON t2.id = t.retweet_id or t2.id = t.in_reply_to
-            JOIN twitter_user u2 ON t2.user_id = u2.id""")
+            JOIN twitter_user u2 ON t2.user_id = u2.idORDER BY u.followers_count DESC
+            LIMIT %d""", (limit, ))
         return cur.fetchall()
 
--- user_graph/Main.py
+++ user_graph/Main.py
@@ -4,6 +4,7 @@ from user_graph.UserGraphGenerator import UserGraphGenerator
 if __name__ == "__main__":
     database = Database()
     user_graph_generator = UserGraphGenerator()
-    user_graph_generator.generate_csv_file('replay_to.csv', database.get_users_in_reply_to_connection())
-    user_graph_generator.generate_csv_file('retweet.csv', database.get_users_retweet_connection())
-    user_graph_generator.generate_csv_file('all.csv', database.get_users_retweet_or_reply_to_connection())
+    limit = 10000
+    user_graph_generator.generate_csv_file('replay_to.csv', database.get_users_in_reply_to_connection(limit))
+    user_graph_generator.generate_csv_file('retweet.csv', database.get_users_retweet_connection(limit))
+    user_graph_generator.generate_csv_file('all.csv', database.get_users_retweet_or_reply_to_connection(limit))
