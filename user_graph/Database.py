c18db603e7b965890a49180104a4ab8d4291f1d6
Ucash <ucashek@gmail.com>
2015-12-01 20:40:59 +0100

    add limit

--- user_graph/Database.py
+++ user_graph/Database.py
@@ -24,7 +24,7 @@ class Database:
             JOIN tweet t2 ON t2.id = t.retweet_id
             JOIN twitter_user u2 ON t2.user_id = u2.id
             ORDER BY u.followers_count DESC
-            LIMIT %d""", (limit, ))
+            LIMIT %s""", (limit, ))
         return cur.fetchall()
 
     def get_users_retweet_or_reply_to_connection(self, limit):
@@ -34,6 +34,6 @@ class Database:
             JOIN tweet t ON t.user_id = u.id
             JOIN tweet t2 ON t2.id = t.retweet_id or t2.id = t.in_reply_to
             JOIN twitter_user u2 ON t2.user_id = u2.idORDER BY u.followers_count DESC
-            LIMIT %d""", (limit, ))
+            LIMIT %s""", (limit, ))
         return cur.fetchall()
 
